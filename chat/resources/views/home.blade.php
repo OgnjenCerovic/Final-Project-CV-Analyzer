3110086ac2d63a56a8b42fd1cfc83b67d9c42d8d
OgnjenCerovic <168226691+OgnjenCerovic@users.noreply.github.com>
2025-04-26 21:21:56 +0200

    chat
    chat

---
+++ chat/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>Welcome to Our Website</h1>
+            <p>This is the homepage of our site.</p>
+        </div>
+    </div>
+
+@endsection
